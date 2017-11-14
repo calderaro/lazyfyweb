830ab5f478053a06490801d52ff7b104144bb5b7
Angel calderaro <calderaro94@gmail.com>
2017-11-13 18:40:58 -0600

    clean

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
 import React from 'react'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Home from './containers/Home/'
-import Artists from './containers/Artistas/'
-import ArtistsForm from './containers/Artistas/Form'
-import Albums from './containers/Albums/'
-import AlbumsForm from './containers/Albums/Form'
-import Songs from './containers/Songs/'
-import SongsForm from './containers/Songs/Form'
-import Login from './containers/Login/'
-import Signup from './containers/Signup/'
 
 const App = () => (
   <Router>
     <Switch>
       <Route exact path='/' component={Home} />
-      <Route exact path='/artists' component={Artists} />
-      <Route exact path='/artists/new' component={ArtistsForm} />
-      <Route exact path='/artists/:id' component={ArtistsForm} />
-
-      <Route exact path='/albums' component={Albums} />
-      <Route exact path='/albums/new' component={AlbumsForm} />
-      <Route exact path='/albums/:id' component={AlbumsForm} />
-
-      <Route exact path='/songs' component={Songs} />
-      <Route exact path='/songs/new' component={SongsForm} />
-      <Route exact path='/songs/:id' component={SongsForm} />
-
-      <Route exact path='/albums' component={Albums} />
-      <Route exact path='/songs' component={Albums} />
-      <Route exact path='/login' component={Login} />
-      <Route exact path='/signup' component={Signup} />
     </Switch>
   </Router>
 )
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,19 +1,5 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
-import Layout from '../../components/Layout/'
 
-class Home extends React.Component {
-  componentDidMount = () => {
-    document.title = 'Lazyfy'
-  }
-  render = () => (
-    <Layout>
-      <div className="text-center">
-        <h1>Bienvenido a Lazyfy</h1>
-        <p>Contenidos Landing Page</p>
-      </div>
-    </Layout>
-  )
-}
+const Home = props => (<div> Lazify </div>)
 
 export default Home
